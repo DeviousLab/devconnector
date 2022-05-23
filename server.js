bd5b803a19f567f57428afe987d1d9309309b069
DeviousLab <deviouslab@gmail.com>
2022-05-23 23:57:08 +0400

    Setup express routes and MongoDB connection
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const app = express();
 
+const connectDB = require('./config/db');
+connectDB();
+
 const PORT = process.env.PORT || 8000;
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
