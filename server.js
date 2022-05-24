de33512b59cd5afeaec40efa70b5303c0a354a49
DeviousLab <deviouslab@gmail.com>
2022-05-24 23:28:06 +0400

    Implemented method to register a user
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 8000;
 
 const connectDB = require('./config/db');
 connectDB();
 
-const PORT = process.env.PORT || 8000;
-
+app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
