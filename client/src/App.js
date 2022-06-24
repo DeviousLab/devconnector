632801159e799e818ba883b2634181268f6c153a
DeviousLab <deviouslab@gmail.com>
2022-06-24 22:58:33 +0400

    Revert "Setup basic frontend"
    This reverts commit 4f92519f5a10b3e82fd31df60739017d4c104696.

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,24 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
+import logo from './logo.svg';
 import './App.css';
-import Navbar from './components/layouts/Navbar';
-import Landing from './components/layouts/Landing';
-import Login from './components/auth/Login';
-import Register from './components/auth/Register';
 
 function App() {
   return (
-    <>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-        </Routes>
-      </Router>
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
