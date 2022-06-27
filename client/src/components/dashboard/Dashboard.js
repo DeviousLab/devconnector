7970f9d2019e9385a1055e5e4dfb67f2e92251f1
DeviousLab <deviouslab@gmail.com>
2022-06-28 00:29:47 +0400

    Implemented method to delete education, experience and accounts
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -8,8 +8,9 @@ import Spinner from '../layouts/Spinner'
 import { DashboardActions } from './DashboardActions'
 import Experience from './Experience'
 import Education from './Education'
+import { deleteAccount } from '../../actions/profile'
 
-const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, loading } }) => {
+const Dashboard = ({ getCurrentProfile, deleteAccount, auth: { user }, profile: { profile, loading } }) => {
   useEffect(() => {
     getCurrentProfile()
   }, [getCurrentProfile])
@@ -25,7 +26,10 @@ const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, load
         <Experience experience={profile.experience} />
         <Education education={profile.education} />
         <div className="my-2">
-          <button className="btn btn-danger" >
+          <button
+            onClick={() => deleteAccount()}
+            className="btn btn-danger"
+          >
             <i className="fas fa-user-minus" /> Delete My Account
           </button>
         </div>
@@ -44,7 +48,8 @@ const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, load
 Dashboard.propTypes = {
   getCurrentProfile: PropTypes.func.isRequired,
   auth: PropTypes.object.isRequired,
-  profile: PropTypes.object.isRequired
+  profile: PropTypes.object.isRequired,
+  deleteAccount: PropTypes.func.isRequired
 }
 
 const mapStateToProps = state => ({
@@ -52,4 +57,4 @@ const mapStateToProps = state => ({
   profile: state.profile
 })
 
-export default connect(mapStateToProps, { getCurrentProfile })(Dashboard)
+export default connect(mapStateToProps, { getCurrentProfile, deleteAccount })(Dashboard)
--- client/src/components/dashboard/Education.js
+++ client/src/components/dashboard/Education.js
@@ -3,7 +3,9 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Moment from 'react-moment';
 
-const Education = ({ education }) => {
+import { deleteEducation } from '../../actions/profile';
+
+const Education = ({ education, deleteEducation }) => {
   const educations = education.map((edu) => (
     <tr key={edu._id}>
       <td>{edu.school}</td>
@@ -14,6 +16,7 @@ const Education = ({ education }) => {
       </td>
       <td>
         <button
+          onClick={() => deleteEducation(edu._id)}
           className="btn btn-danger"
         >
           Delete
@@ -42,6 +45,7 @@ const Education = ({ education }) => {
 
 Education.propTypes = {
   education: PropTypes.array.isRequired,
+  deleteEducation: PropTypes.func.isRequired,
 };
 
-export default connect(null)(Education);
+export default connect(null, { deleteEducation })(Education);
