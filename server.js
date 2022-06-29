d4b1e5997c44a6938122634c8be64c36db5d043c
DeviousLab <deviouslab@gmail.com>
2022-06-30 01:03:45 +0400

    Prepare for heroku deployment
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

--- server.js
+++ server.js
@@ -3,18 +3,23 @@ const app = express();
 require('dotenv').config()
 const PORT = process.env.PORT || 8000;
 const connectDB = require('./config/db');
+const path = require('path');
+
 connectDB();
 
 app.use(express.json({ extended: false }));
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
 
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
+
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.length('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
